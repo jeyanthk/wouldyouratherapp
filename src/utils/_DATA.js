bacf0fc8f5167d633ec39e7bfb367840cb0ffaf4
Kothari <jeyanth.kothari@homesite.com>
2018-10-23 16:36:38 -0400

    Changes per reviews

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: 'https://pbs.twimg.com/profile_images/982796605425303552/phhp5grt_400x400.jpg',
+    avatarURL: 'https://pbs.twimg.com/profile_images/1039352369232789504/M7tgq-c8_400x400.jpg',
     answers: {
       '8xf0y6ziyjabvozdd253nd': 'optionOne',
       '6ni6ok3ym7mf1p33lnez': 'optionOne',
